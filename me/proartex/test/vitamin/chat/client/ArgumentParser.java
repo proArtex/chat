2a3e0dcc481765894fe1088d6dccf432365097af
proArtex <proartex@mail.ru>
2015-04-29 22:24:25 +0500

    ArgumentParser

---
+++ me/proartex/test/vitamin/chat/client/ArgumentParser.java
@@ -0,0 +1,40 @@
+package me.proartex.test.vitamin.chat.client;
+
+public class ArgumentParser {
+    private final String[] args;
+    private String host;
+    private int port;
+
+    public ArgumentParser(String[] args) {
+        this.args = args;
+        parseParams();
+        setDefaultsIfEmpty();
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    private void parseParams() {
+        for (int i = 0; i < args.length; i++) {
+            if ("-host".equals(args[i])) {
+                host = args[i + 1];
+            }
+            else if ("-port".equals(args[i])) {
+                port = Integer.parseInt(args[i + 1]);
+            }
+        }
+    }
+
+    private void setDefaultsIfEmpty() {
+        if (host == null)
+            host = "localhost";
+
+        if (port == 0)
+            port = 9993;
+    }
+}
